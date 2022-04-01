efeb6a5bc93147c355afc03969ad9b9c04f532fd
Waris Khan <waris.a@cisinlabs.com>
2022-04-01 19:51:28 +0530

    build update

---
+++ build/js/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	
+	//top-slider
+	$('.top-baner-wrap .owl-carousel').owlCarousel({
+		items:1,
+	    loop:false,
+		dots:false,
+	    nav:true,
+		smartSpeed:700
+	});
+
+
+});
+
