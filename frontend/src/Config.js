77c9618b8abd5f5fb037adaae8c12983a37e153d
Prince Gupta <prince.g@cisinlabs.com>
2022-05-26 16:23:49 +0530

    change http

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://52.202.111.97:3000";
+export const API_URL = "http://52.202.111.97:3000";
 export const STRIPE_KEY = "pk_test_51K4PwzSIO6cxIMLSw8CgeRAEzlUlNxw3TJLnMyg8obnjKbTs9ZOYb6OXohopjUyLX9tqMaeWWpA4ucCJlGr72CHc00GTwlIRrM";
