1800175bbecb15722ed32a71efe168ab145abf0e
Prince Gupta <prince.g@cisinlabs.com>
2022-06-06 15:45:14 +0530

    added countdown page

--- frontend/src/frontend/ForgetPassword.jsx
+++ frontend/src/frontend/ForgetPassword.jsx
@@ -6,96 +6,98 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
 export default function ForgetPassword({ setRole }) {
-    let history = useNavigate();
+  let history = useNavigate();
 
-    const [email, setEmail] = useState("");
+  const [email, setEmail] = useState("");
 
-    useEffect(() => {
+  useEffect(() => {}, []);
 
-    }, []);
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
-    const validateEmail = (email) => {
-        return String(email)
-            .toLowerCase()
-            .match(
-                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  async function handleLogin(event) {
+    event.preventDefault();
+    if (!email) {
+      toast.warn("Please Enter email");
+    } else if (!validateEmail(email)) {
+      toast.warn("Please Enter valid email");
+    } else {
+      const option = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+        }),
+      };
+      await fetch(`${API_URL}/user/forget-password`, option)
+        .then(async (response) => {
+          const res = await response.json();
+          if (res.success === true) {
+            toast.success(
+              "Email to generate new password sent successfully !!!"
             );
-    };
-
-    async function handleLogin(event) {
-        event.preventDefault();
-        if (!email) {
-            toast.warn("Please Enter email");
-        } else if (!validateEmail(email)) {
-            toast.warn("Please Enter valid email");
-        } else {
-            const option = {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    email: email
-                }),
-            };
-            await fetch(`${API_URL}/user/forget-password`, option)
-                .then(async (response) => {
-                    const res = await response.json();
-                    if (res.success === true) {
-                        toast.success("Email to generate new password sent successfully !!!");
-                    }
-                    else {
-                        toast.error(res?.msg);
-                    }
-                })
-                .catch((e) => {
-                    console.log(e);
-                });
-        }
+          } else {
+            toast.error(res?.msg);
+          }
+        })
+        .catch((e) => {
+          console.log(e);
+        });
     }
-    return (
-        <div>
-            <Header activeHeader={"Login"} />
-            <div className="header-btm">
-                <img src="images/header.png" alt="header-image"></img>
-            </div>
-            <div className="login-sec2">
-                <div className="cust-container cust-container-login">
-                    <div className="inner-login-sec">
-                        {/* <a href="#" className="logo"><img src="images/Logo.png" /></a> */}
-                        <div className="dark-box">
-                            <div className="heading-sec text-center">
-                                <h2>Enter Email</h2>
-                                <img
-                                    src="images/services.png"
-                                    alt="services-image"
-                                    className="border-b"
-                                />
-                            </div>
+  }
+  return (
+    <div>
+      <Header activeHeader={"Login"} />
+      <div className="header-btm">
+        <img src="images/header.png" alt="header-image"></img>
+      </div>
+      <div className="login-sec2">
+        <div className="cust-container cust-container-login">
+          <div className="inner-login-sec">
+            {/* <a href="#" className="logo"><img src="images/Logo.png" /></a> */}
+            <div className="dark-box">
+              <div className="heading-sec text-center">
+                <h2>Enter Email</h2>
+                <img
+                  src="images/services.png"
+                  alt="services-image"
+                  className="border-b"
+                />
+              </div>
 
-                            <form>
-                                <div className="form-group">
-                                    <input
-                                        type="Email"
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        className="form-control"
-                                        placeholder="Email"
-                                    />
-                                </div>
-
-                                <button
-                                    type="submit"
-                                    className="btn btn-more"
-                                    onClick={handleLogin}
-                                >
-                                    Submit
-                                </button>
-                            </form>
-                        </div>
-                    </div>
+              <form>
+                <div className="form-group">
+                  <input
+                    type="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="form-control"
+                    placeholder="Email"
+                  />
                 </div>
+
+                <button
+                  type="submit"
+                  className="btn btn-more"
+                  onClick={handleLogin}
+                >
+                  Submit
+                </button>
+                <p>
+                  <Link to="/login">Back To Login</Link>
+                </p>
+              </form>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
